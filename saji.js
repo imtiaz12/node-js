8f649427d8381c57c822fccb094af95c21ee51d3
imtiaz <imtiazking1@gmail.com>
2018-07-12 23:00:02 +0500

    information

---
+++ saji.js
@@ -0,0 +1,27 @@
+var array1=["ali"];
+var array2=["saqib"];
+var array3=["usama"];
+var alirecord=["1,ali,islamabd"];
+var saqibrecord=["2,saqib,lahore"];
+var usamarecord=["3,usama,karachi"];
+array1.map(function(array1){
+console.log(array1);
+})
+console.log(alirecord);
+
+array2.map(function(array2){
+
+    console.log(array2);
+    })
+    console.log(saqibrecord);
+    array3.map(function(array3){
+        console.log(array3);
+        })
+        console.log(usamarecord);
+        array1.push("friend","friends");
+        console.log(array1);
+        array1.pop("ali");
+        console.log(array1);
+
+        
+        
