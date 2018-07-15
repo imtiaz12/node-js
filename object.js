126b2f42fda280ebf778d50f094b41d94144b2c2
imtiaz <imtiazking1@gmail.com>
2018-07-15 22:58:23 +0500

    object

---
+++ object.js
@@ -0,0 +1,11 @@
+var data={name:"imtiaz",id:4,room:"14th",city:"lahore",
+start:function(){
+   console.log("Student Record ");
+   
+}
+}
+data.start();
+console.log(data.name);
+console.log(data.id);
+console.log(data.room);
+console.log(data.city);
